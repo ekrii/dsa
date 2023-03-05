a1e2982a27bd2c1806348bf8e460d8533804635c
EKRI <ekrii@protonmail.com>
2023-03-05 22:49:36 +1100

    .

---
+++ algos/fib.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+int fib(int n) { 
+    int a = 0;
+    int b = 1;
+
+    for (int i = 0; i < n; i++) { 
+        int tmp = a + b;
+        a = b;
+        b = tmp;
+        
+    }
+    return a;
+}
