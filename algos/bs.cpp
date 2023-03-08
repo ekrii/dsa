46a79ac020634d4f49f7715a213b492dc4f63f0a
EKRI <ekrii@protonmail.com>
2023-03-08 22:46:26 +1100

    .

---
+++ algos/bs.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int bs(vector<int> v, int k) { 
+    int l = - 1;
+    int r = v.size() - 1;
+
+    while(r - l > 1) { 
+        int m = l + (l - 1) / 2;
+        if(k < v[m]) { 
+            r = m;
+        }
+        else { 
+            l = m;
+        }
+    }
+    return k;
+}
+
+
+
+
+
